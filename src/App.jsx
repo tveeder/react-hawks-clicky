86bf140c60937e31eb92c5e770f45a6702be7c01
tveeder <tveeder@live.com>
2019-09-01 14:57:55 -0500

    adding text and adding to Readme

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import cards from "./cards.json";
 import "./App.css";
 
 class App extends Component {
-  // Setting this.state.cards to the cards json array
+  // Setting this.state.cards to the cards json array of Blackhawk players
   state = {
     cards,
     score: 0,
@@ -47,7 +47,11 @@ class App extends Component {
   render() {
     return (
       <Wrapper>
-        <Header score={this.state.score} highscore={this.state.highscore}>Blackhawks Clicky Game</Header>
+        <Header score={this.state.score} highscore={this.state.highscore}>Blackhawks Clicky Game  </Header>
+
+        <p>Try to click on every Blackhawk Player once. Once you click a player the grid will shuffle.
+Try not to click the same Blackhawk Player twice or the game will start all over.  </p>
+
         {this.state.cards.map(card => (
           <Card
             clickCount={this.clickCount}
